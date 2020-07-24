193d34f5f51e813854981c2fa7780efa8d1de1de
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-07-24 18:20:25 -0300

    master: Model Definition

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,32 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
+  @Column('varchar')
   type: 'income' | 'outcome';
 
+  @Column('decimal', { precision: 10, scale: 2 })
   value: number;
 
+  @Column('uuid')
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
