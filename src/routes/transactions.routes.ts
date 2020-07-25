c9a4fd22e1bbc6d19f437c7c960dcd60034e259e
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-07-25 14:07:27 -0300

    master: Delete Transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -38,7 +38,13 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransactions = new DeleteTransactionService();
+
+  await deleteTransactions.execute({ transaction_id: id });
+
+  response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,28 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface ServiceRequest {
+  transaction_id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ transaction_id }: ServiceRequest): Promise<void> {
+    // Check if there is transaction with the given id
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transactionFound = await transactionsRepository.findOne(
+      transaction_id,
+    );
+
+    if (!transactionFound) {
+      throw new AppError('Transaction not found.');
+    }
+
+    // delete transaction
+    await transactionsRepository.delete(transaction_id);
   }
 }
 
